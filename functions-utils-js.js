723286e3ae9589323e938f3a96b6abbe850ebffb
DuckTech <yuri13royer@gmail.com>
2023-02-19 11:37:45 -0300

    update

--- functions-utils-js.js
+++ functions-utils-js.js
@@ -1,4 +1,4 @@
-//função formatar valores em brl para o padrão pt-br colocando o R$ e separando os milhares com ponto e as casas decimais com vírgula 
+//função formatar valores em brl para o padrão pt-br colocando o R$
 function formatarMoeda(valor) {
-    return "R$ " + valor.toFixed(2).replace(".", ",").replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
+    return valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 }
