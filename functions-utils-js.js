e24d7a620c82cf726ff7efd1cc8bcb9e2e75e6ab
DuckTech <yuri13royer@gmail.com>
2023-02-19 12:05:15 -0300

    Update functions-utils-js.js

--- functions-utils-js.js
+++ functions-utils-js.js
@@ -1,5 +1,11 @@
 //função formatar valores em brl para o padrão pt-br colocando o R$
 function formatarMoeda(atual) {
-    var f = atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-    return f;
-  }
+  var f = atual.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+  return f;
+}
+
+//funcao padrão para salvar dado em localstorage passando dois parametros o nome do campo e o valor
+function saveInLocalStorage(param, value) {
+  localStorage.setItem(param, value);
+  console.log(value);
+}
